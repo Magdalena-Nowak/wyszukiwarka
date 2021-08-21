4bf2a3e5aa12c7717a5fabb7c157ef3ba0e83a02
Magda <meg.nowak27@gmail.com>
2021-08-21 18:58:17 +0200

    search option

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const listItems = document.querySelectorAll("li");
 const textInput = document.querySelector(".form-control");
-const regex = /[a-z]/g;
+const list = document.querySelector(".list-group-flush");
+let allDrinks = [];
+let allMatchingDrinks = [];
 
-console.log(textInput.value);
+listItems.forEach(item => allDrinks.push(item.textContent));
 
-const checkingDrinks = () => {
-  let search = textInput.value.toLowerCase();
-  let matchItem;
-};
+const checkingSearchingMatch = () => {
+    if (textInput.value) {
+    const newListItems = document.querySelectorAll("li");
+    newListItems.forEach(item => item.remove());
+    allMatchingDrinks = [];
+    const userSearch = textInput.value.toLowerCase();
+allDrinks.forEach((item) => {
+    const smallDrink = item.toLowerCase();
+    if (smallDrink.match(userSearch)) {
+        allMatchingDrinks.push(smallDrink);
+    }
+});
+allMatchingDrinks.forEach((item) => {
+    const newItem = document.createElement('li');
+    newItem.classList.add("list-group-item", "p-3");
+    newItem.textContent = item;
+    list.append(newItem);
+})
+} else {
+    const newListItems = document.querySelectorAll("li");
+    newListItems.forEach(item => item.remove());
+    allMatchingDrinks = [];
+    allDrinks.forEach((item) => {
+        const newItem = document.createElement('li');
+        newItem.classList.add("list-group-item", "p-3");
+        newItem.textContent = item;
+        list.append(newItem);
+    })  
+}
+}
 
-textInput.addEventListener("input", checkingDrinks);
+
+
+textInput.addEventListener("input", checkingSearchingMatch);
